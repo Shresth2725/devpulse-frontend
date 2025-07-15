d2e359cf0975b9957710db2db03dd516838b0e4c
Shresth <2725shresth@gmail.com>
2025-07-15 09:41:32 +0530

    added signUp feature

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -8,13 +8,30 @@ import { baseUrl } from "../../utilis/constant";
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(true);
+  const [firstName, setFirstName] = useState("Shresth");
+  const [lastName, setLastName] = useState("Srivastava");
   const [email, setEmail] = useState("Shresth@gmail.com");
   const [password, setPassword] = useState("Shresth@1234");
   const [error, setError] = useState("");
 
+  const handleFirstName = (e) => {
+    const value = e.target.value;
+    setFirstName(value);
+  };
+
+  const handleLastName = (e) => {
+    const value = e.target.value;
+    setLastName(value);
+  };
+
   const handleEmailInput = (e) => {
     const value = e.target.value;
     setEmail(value);
+  };
+
+  const toggleLoginSignin = () => {
+    setIsLogin(!isLogin);
   };
 
   const handlePasswordInput = (e) => {
@@ -40,6 +57,26 @@ const Login = () => {
     }
   };
 
+  const handleSignIn = async () => {
+    try {
+      const res = await axios.post(
+        baseUrl + "/signup",
+        {
+          firstName: firstName,
+          lastName: lastName,
+          emailId: email,
+          password: password,
+        },
+        { withCredentials: true }
+      );
+      // console.log(res.data.data);
+      dispatch(addUser(res.data.data));
+      navigate("/profile");
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <div className="flex justify-center my-10">
       <div className="card w-96 bg-base-300 card-md shadow-sm">
@@ -48,6 +85,34 @@ const Login = () => {
             Login
           </h2>
           <div>
+            {!isLogin && (
+              <div>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">
+                    What is your First Name?
+                  </legend>
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder="Type here"
+                    onChange={handleFirstName}
+                    value={firstName}
+                  />
+                </fieldset>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">
+                    What is your Last Name?
+                  </legend>
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder="Type here"
+                    onChange={handleLastName}
+                    value={lastName}
+                  />
+                </fieldset>
+              </div>
+            )}
             <fieldset className="fieldset">
               <legend className="fieldset-legend">What is your Email?</legend>
               <input
@@ -73,10 +138,26 @@ const Login = () => {
           </div>
           <p className="text-red-500 font-bold">{error}</p>
           <div className="justify-center card-actions pt-2">
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
-            </button>
+            {isLogin && (
+              <button className="btn btn-primary" onClick={handleLogin}>
+                Login
+              </button>
+            )}
+            {!isLogin && (
+              <button className="btn btn-primary" onClick={handleSignIn}>
+                SignUp
+              </button>
+            )}
           </div>
+          <p className="text-center text-gray-600 mt-3">
+            {isLogin ? "New user?" : "Already have an account?"}{" "}
+            <span
+              onClick={toggleLoginSignin}
+              className="text-blue-600 cursor-pointer hover:underline"
+            >
+              {isLogin ? "Sign Up" : "Login"}
+            </span>
+          </p>
         </div>
       </div>
     </div>
