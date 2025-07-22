dec520d28b8835d424be7b07d0dcef2362b487d7
Shresth <2725shresth@gmail.com>
2025-07-22 23:41:23 +0530

    bug fix

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -4,12 +4,12 @@ import { baseUrl } from "../../utilis/constant";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../../utilis/feedSlice";
 import FeedCard from "./FeedCard";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const Feed = () => {
   const dispatch = useDispatch();
   const userFeed = useSelector((store) => store.Feed);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const getFeed = async () => {
     if (userFeed) return;
@@ -18,7 +18,7 @@ const Feed = () => {
       dispatch(addFeed(res.data.data));
     } catch (err) {
       console.error(err);
-      navigate("/error");
+      // navigate("/error");
     }
   };
 
