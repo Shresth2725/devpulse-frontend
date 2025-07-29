89c50a45421904f079758ef046c015cd16d8c541
Shresth <2725shresth@gmail.com>
2025-07-29 21:17:52 +0530

    footer ui change

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="z-10 footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 fixed bottom-0">
-        <aside>
-          <p>
-            Copyright © {new Date().getFullYear()} - All right reserved by
-            @Shresth
-          </p>
-        </aside>
-      </footer>
-    </div>
+    <footer className="z-10 footer footer-center bg-base-300 text-base-content p-4">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by
+          @Shresth
+        </p>
+      </aside>
+    </footer>
   );
 };
 
