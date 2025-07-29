8e5eac5d9a6ba437f3883d21695f1444d196b916
Shresth <2725shresth@gmail.com>
2025-07-29 20:44:48 +0530

    comments removed

--- src/components/setting/Modes.jsx
+++ src/components/setting/Modes.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleMode } from "../../utilis/settingSlice"; // Adjust path
+import { toggleMode } from "../../utilis/settingSlice";
 
 const Modes = () => {
   const theme = useSelector((state) => state.Setting);
