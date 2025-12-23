9cfda91f15a15291e03e9b392fe6deab74c046bf
Shresth <152704478+Shresth2725@users.noreply.github.com>
2025-12-23 20:28:14 +0530

    Fix import case for Post component

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import Chat from "./components/chat/Chat";
 import ConnectionsChat from "./components/chat/ConnectionChat";
 import ChatLayout from "./components/chat/ChatLayout";
 import Premium from "./components/premium/Premium";
-import Post from "./components/post/post";
+import Post from "./components/post/Post";
 
 const App = () => {
   return (
